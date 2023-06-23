d13df8f9636e16b5d54bfa64b8a2d0324d94db4c
huntfeng <spacecurvature@gmail.com>
2023-06-23 12:22:11 -0600

    show alpha when no buffers

--- mappings.lua
+++ mappings.lua
@@ -17,7 +17,16 @@ return {
       desc = "Previous buffer",
     },
     ["<Cr>"] = { "o<Esc>", desc = "New line" },
-    -- neotree buffer navigation
+    -- show Alpha when no buffer
+    ["<leader>c"] = {
+      function()
+        local bufs = vim.fn.getbufinfo { buflisted = true }
+        require("astronvim.utils.buffer").close(0)
+        print("bufs", bufs)
+        if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
+      end,
+      desc = "Close buffer",
+    }
   },
   v = {
     ["j"] = "gj",
