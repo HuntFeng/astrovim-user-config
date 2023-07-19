04c726be8dd7607a288c4eab541c144cb58a5476
huntfeng <spacecurvature@gmail.com>
2023-07-19 10:55:59 -0600

    suppress warnings when compiled

--- plugins/vimtex.lua
+++ plugins/vimtex.lua
@@ -2,7 +2,8 @@ return {
   "lervag/vimtex",
   config = function()
     vim.g.vimtex_syntax_enabled = 1
-    vim.g.vimtex_view_general_viewer = "/home/huntfeng/sioyek/Sioyek-x86_64.AppImage"
+    vim.g.vimtex_quickfix_open_on_warning = 0
+    vim.g.vimtex_view_general_viewer = "sioyek"
     vim.g.vimtex_compiler_progname = "nvr"
     -- %1: file, %2: line number
     local options =
